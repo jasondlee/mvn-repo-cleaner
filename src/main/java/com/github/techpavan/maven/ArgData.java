3924603b40304dd74c7484c58ed85d0c7e177ea9
Jason Lee <jason@steeplesoft.com>
2021-10-01 17:26:14 -0500

    Quarkification complete.
    Now to figure out packaging

--- src/main/java/com/github/techpavan/maven/ArgData.java
+++ src/main/java/com/github/techpavan/maven/ArgData.java
@@ -9,146 +9,66 @@ package com.github.techpavan.maven;
 import java.text.ParseException;
 import java.util.List;
 
-import com.beust.jcommander.IStringConverter;
-import com.beust.jcommander.Parameter;
 import org.apache.commons.lang3.time.DateUtils;
+import picocli.CommandLine;
 
 public class ArgData {
 
-    @Parameter(names = {"--path", "-p"}, description = "Path to m2 directory, if using a custom path.")
-    private String m2Path;
+    @CommandLine.Option(names = {"--path", "-p"},
+            description = "Path to m2 directory, if using a custom path.")
+    protected String m2Path;
 
-    @Parameter(names = {"--downloadedBefore", "-db"}, description = "Delete all libraries (even if latest version) downloaded on or before this date (MM-DD-YYYY).", converter = DateToMillisConverter.class)
-    private Long downloadedBefore = 0L;
+    @CommandLine.Option(names = {"--downloadedBefore", "-db"},
+            description = "Delete all libraries (even if latest version) downloaded on or before this date (MM-DD-YYYY).",
+            converter = DateToMillisConverter.class)
+    protected Long downloadedBefore = 0L;
 
-    @Parameter(names = {"--accessedBefore", "-ab"}, description = "Delete all libraries (even if latest version) last accessed on or before this date (MM-DD-YYYY).", converter = DateToMillisConverter.class)
-    private Long accessedBefore = 0L;
+    @CommandLine.Option(names = {"--accessedBefore", "-ab"},
+            description = "Delete all libraries (even if latest version) last accessed on or before this date (MM-DD-YYYY).",
+            converter = DateToMillisConverter.class)
+    protected Long accessedBefore = 0L;
 
-    @Parameter(names = {"--ignoreArtifacts", "-ia"}, description = "Comma separated list of groupId:artifactId combination to be ignored."/*, converter = StringToListConverter.class*/)
-    private List<String> ignoreArtifacts;
+    @CommandLine.Option(names = {"--ignoreArtifacts", "-ia"},
+            description = "Comma separated list of groupId:artifactId combination to be ignored."
+            /*, converter = StringToListConverter.class*/)
+    protected List<String> ignoreArtifacts;
 
-    @Parameter(names = {"--ignoreGroups", "-ig"}, description = "Comma separated list of groupIds (full or part) to be ignored."/*, converter = StringToListConverter.class*/)
-    private List<String> ignoreGroups;
+    @CommandLine.Option(names = {"--ignoreGroups", "-ig"},
+            description = "Comma separated list of groupIds (full or part) to be ignored."
+            /*, converter = StringToListConverter.class*/)
+    protected List<String> ignoreGroups;
 
-    @Parameter(names = {"--deleteAllSnapshots", "-dsn"}, description = "Delete all snapshots irrespective of being latest.")
-    private boolean deleteAllSnapshots;
+    @CommandLine.Option(names = {"--deleteAllSnapshots", "-dsn"},
+            description = "Delete all snapshots irrespective of being latest.")
+    protected boolean deleteAllSnapshots;
 
-    @Parameter(names = {"--deleteSource", "-dsr"}, description = "Delete sources for all libraries.")
-    private boolean deleteSource;
+    @CommandLine.Option(names = {"--deleteSource", "-dsr"},
+            description = "Delete sources for all libraries.")
+    protected boolean deleteSource;
 
-    @Parameter(names = {"--deleteJavadoc", "-djd"}, description = "Delete javadocs for all libraries.")
-    private boolean deleteJavadoc;
+    @CommandLine.Option(names = {"--deleteJavadoc", "-djd"},
+            description = "Delete javadocs for all libraries.")
+    protected boolean deleteJavadoc;
 
-    @Parameter(names = {"--forceArtifacts", "-fa"}, description = "Comma separated list of groupId:artifactId combination to be deleted."/*, converter = StringToListConverter.class*/)
-    private List<String> forceArtifacts;
+    @CommandLine.Option(names = {"--forceArtifacts", "-fa"},
+            description = "Comma separated list of groupId:artifactId combination to be deleted."
+            /*, converter = StringToListConverter.class*/)
+    protected List<String> forceArtifacts;
 
-    @Parameter(names = {"--forceGroups", "-fg"}, description = "Comma separated list of groupIds (full or part) to be deleted."/*, converter = StringToListConverter.class*/)
-    private List<String> forceGroups;
+    @CommandLine.Option(names = {"--forceGroups", "-fg"},
+            description = "Comma separated list of groupIds (full or part) to be deleted."
+            /*, converter = StringToListConverter.class*/)
+    protected List<String> forceGroups;
 
-    @Parameter(names = {"--dryrun", "-dr"}, description = "Do not delete files, just simulate and print result.")
-    private boolean dryRun;
+    @CommandLine.Option(names = {"--dryrun", "-dr"},
+            description = "Do not delete files, just simulate and print result.")
+    protected boolean dryRun;
 
-    @Parameter(names = {"--retainOld", "-ro"}, description = "Retain the artifacts even if old versions. Only process the configured inputs.")
-    private boolean retainOld;
+    @CommandLine.Option(names = {"--retainOld", "-ro"},
+            description = "Retain the artifacts even if old versions. Only process the configured inputs.")
+    protected boolean retainOld;
 
-    public String getM2Path() {
-        return m2Path;
-    }
-
-    public void setM2Path(String m2Path) {
-        this.m2Path = m2Path;
-    }
-
-    public Long getDownloadedBefore() {
-        return downloadedBefore;
-    }
-
-    public void setDownloadedBefore(Long downloadedBefore) {
-        this.downloadedBefore = downloadedBefore;
-    }
-
-    public Long getAccessedBefore() {
-        return accessedBefore;
-    }
-
-    public void setAccessedBefore(Long accessedBefore) {
-        this.accessedBefore = accessedBefore;
-    }
-
-    public List<String> getIgnoreArtifacts() {
-        return ignoreArtifacts;
-    }
-
-    public void setIgnoreArtifacts(List<String> ignoreArtifacts) {
-        this.ignoreArtifacts = ignoreArtifacts;
-    }
-
-    public List<String> getIgnoreGroups() {
-        return ignoreGroups;
-    }
-
-    public void setIgnoreGroups(List<String> ignoreGroups) {
-        this.ignoreGroups = ignoreGroups;
-    }
-
-    public boolean isDeleteAllSnapshots() {
-        return deleteAllSnapshots;
-    }
-
-    public void setDeleteAllSnapshots(boolean deleteAllSnapshots) {
-        this.deleteAllSnapshots = deleteAllSnapshots;
-    }
-
-    public boolean isDeleteSource() {
-        return deleteSource;
-    }
-
-    public void setDeleteSource(boolean deleteSource) {
-        this.deleteSource = deleteSource;
-    }
-
-    public boolean isDeleteJavadoc() {
-        return deleteJavadoc;
-    }
-
-    public void setDeleteJavadoc(boolean deleteJavadoc) {
-        this.deleteJavadoc = deleteJavadoc;
-    }
-
-    public List<String> getForceArtifacts() {
-        return forceArtifacts;
-    }
-
-    public void setForceArtifacts(List<String> forceArtifacts) {
-        this.forceArtifacts = forceArtifacts;
-    }
-
-    public List<String> getForceGroups() {
-        return forceGroups;
-    }
-
-    public void setForceGroups(List<String> forceGroups) {
-        this.forceGroups = forceGroups;
-    }
-
-    public boolean isDryRun() {
-        return dryRun;
-    }
-
-    public void setDryRun(boolean dryRun) {
-        this.dryRun = dryRun;
-    }
-
-    public boolean isRetainOld() {
-        return retainOld;
-    }
-
-    public void setRetainOld(boolean retainOld) {
-        this.retainOld = retainOld;
-    }
-
-    static class DateToMillisConverter implements IStringConverter<Long> {
-
+    static class DateToMillisConverter implements CommandLine.ITypeConverter<Long> {
         @Override
         public Long convert(String value) {
             try {
--- src/main/java/com/github/techpavan/maven/CleanM2.java
+++ src/main/java/com/github/techpavan/maven/CleanM2.java
@@ -25,25 +25,24 @@ import java.util.Set;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.ParameterException;
-import com.beust.jcommander.internal.Lists;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.artifact.versioning.ComparableVersion;
+import picocli.CommandLine;
 
-public class CleanM2 {
-    private static Logger log = Logger.getLogger(CleanM2.class.getCanonicalName());
-    private static Map<DeleteReason, Set<File>> DELETE_MAP = new LinkedHashMap<>();
-    private static Map<String, Set<FileInfo>> PROCESS_MAP = new HashMap<>();
-    private static Map<SkipReason, Set<String>> SKIP_MAP = new HashMap<>();
-    private static List<File> DELETE_FAILURE_LIST = new ArrayList<>();
-    private static ArgData argData = new ArgData();
-    private static List<String> RESTRICTED_FILES = Lists.newArrayList("repository.xml", "_maven.repositories", "_remote.repositories", "m2e-lastUpdated.properties", "resolver-status.properties");
-    private static List<String> RESTRICTED_PATTERN = Lists.newArrayList("maven-metadata-", ".jar.lastUpdated", ".pom.lastUpdated");
-    private static String LS = System.lineSeparator();
+@CommandLine.Command
+public class CleanM2 extends ArgData implements Runnable {
+    private Logger log = Logger.getLogger(CleanM2.class.getCanonicalName());
+    private Map<DeleteReason, Set<File>> DELETE_MAP = new LinkedHashMap<>();
+    private Map<String, Set<FileInfo>> PROCESS_MAP = new HashMap<>();
+    private Map<SkipReason, Set<String>> SKIP_MAP = new HashMap<>();
+    private List<File> DELETE_FAILURE_LIST = new ArrayList<>();
+    private ArgData argData = new ArgData();
+    private List<String> RESTRICTED_FILES = Arrays.asList("repository.xml", "_maven.repositories", "_remote.repositories", "m2e-lastUpdated.properties", "resolver-status.properties");
+    private List<String> RESTRICTED_PATTERN = Arrays.asList("maven-metadata-", ".jar.lastUpdated", ".pom.lastUpdated");
+    private String LS = System.lineSeparator();
 
-    static {
+    {
         DELETE_MAP.put(DeleteReason.FORCED_SNAPSHOT, new HashSet<>());
         DELETE_MAP.put(DeleteReason.FORCED_SOURCE, new HashSet<>());
         DELETE_MAP.put(DeleteReason.FORCED_JAVADOC, new HashSet<>());
@@ -60,17 +59,17 @@ public class CleanM2 {
         SKIP_MAP.put(SkipReason.LATEST, new HashSet<>());
     }
 
-    public static void main(String[] args) {
-        JCommander jCommander = parseInputParams(args);
-        File repoDir = evaluateM2Path(jCommander);
-        String[] filter = argData.isDeleteSource() || argData.isDeleteJavadoc() ? null : new String[]{"pom"};
+    @Override
+    public void run() {
+        File repoDir = evaluateM2Path();
+        String[] filter = deleteSource || deleteJavadoc ? null : new String[]{"pom"};
         FileUtils.listFiles(repoDir, filter, true).forEach(file -> parseAndEvaluate(file));
         processVersion();
         log.info("*********** Files to be deleted ***********");
         DELETE_MAP.forEach((k, v) -> log.info(LS + LS + k + " : " + LS + StringUtils.join(v.stream().sorted().iterator(), LS)));
         log.info(LS + LS + "*********** Files skipped ***********");
         SKIP_MAP.forEach((k, v) -> log.info(LS + LS + k + " : " + LS + StringUtils.join(v.stream().sorted().iterator(), LS)));
-        if (!argData.isDryRun()) {
+        if (!dryRun) {
             log.info(LS + LS + "*********** Beginning to Delete Files ***********");
             deleteMarked();
             log.info(LS + LS + "*********** Deletion Completed ***********");
@@ -81,30 +80,18 @@ public class CleanM2 {
         }
     }
 
-    private static File evaluateM2Path(JCommander jCommander) {
-        String m2Path = defaultString(argData.getM2Path(), concat(USER_HOME, ".m2"));
+    private File evaluateM2Path() {
+        String m2Path = defaultString(this.m2Path, concat(USER_HOME, ".m2"));
         File m2Dir = new File(m2Path);
         File repoDir = new File(m2Path, "repository");
         if (!m2Dir.exists() || !repoDir.exists()) {
             log.severe("Valid Maven repository could not be found. Please provide a valid input.");
-            jCommander.usage();
             System.exit(1);
         }
         return repoDir;
     }
 
-    private static JCommander parseInputParams(String[] args) {
-        JCommander jCommander = JCommander.newBuilder().addObject(argData).build();
-        try {
-            jCommander.parse(args);
-        } catch (ParameterException e) {
-            jCommander.usage();
-            System.exit(1);
-        }
-        return jCommander;
-    }
-
-    private static void deleteMarked() {
+    private void deleteMarked() {
         DELETE_MAP.values().forEach(fileSet -> {
             fileSet.forEach(file -> {
                 if (!FileUtils.deleteQuietly(file)) {
@@ -114,7 +101,7 @@ public class CleanM2 {
         });
     }
 
-    private static void processVersion() {
+    private void processVersion() {
         PROCESS_MAP.forEach((gaId, versionSet) -> {
             String latest = getLatestVersion(versionSet);
             versionSet.forEach(fileInfo -> {
@@ -127,13 +114,13 @@ public class CleanM2 {
         });
     }
 
-    private static String getLatestVersion(Set<FileInfo> versionSet) {
+    private String getLatestVersion(Set<FileInfo> versionSet) {
         List<String> versionList = versionSet.stream().map(f -> f.getVersion()).collect(Collectors.toList());
         String latest = versionList.stream().map(v -> new ComparableVersion(v)).sorted().reduce((a, b) -> b).get().toString();
         return latest;
     }
 
-    private static void parseAndEvaluate(File file) {
+    private void parseAndEvaluate(File file) {
         try {
             if (isRestrictedFile(file.getName())) {
                 SKIP_MAP.get(SkipReason.RESERVED).add(file.getAbsolutePath());
@@ -158,14 +145,14 @@ public class CleanM2 {
 
             BasicFileAttributes attributes = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
             // For below cases, add entire folder instead of just individual files
-            if (attributes.lastAccessTime().toMillis() < argData.getAccessedBefore()) {
+            if (attributes.lastAccessTime().toMillis() < this.accessedBefore) {
                 DELETE_MAP.get(DeleteReason.ACCESS_DATE).add(file.getParentFile());
                 return;
-            } else if (attributes.lastModifiedTime().toMillis() < argData.getDownloadedBefore()) {
+            } else if (attributes.lastModifiedTime().toMillis() < downloadedBefore) {
                 DELETE_MAP.get(DeleteReason.DOWNLOAD_DATE).add(file.getParentFile());
                 return;
             }
-            if (argData.isRetainOld()) {
+            if (retainOld) {
                 SKIP_MAP.get(SkipReason.RETAIN_OLD).add(file.getParentFile().getAbsolutePath());
                 return;
             }
@@ -176,14 +163,14 @@ public class CleanM2 {
         }
     }
 
-    private static boolean isRestrictedFile(String name) {
+    private boolean isRestrictedFile(String name) {
         if (RESTRICTED_FILES.contains(name)) {
             return true;
         }
         return RESTRICTED_PATTERN.stream().filter(pattern -> name.contains(pattern)).count() > 0;
     }
 
-    private static void addToProcessMap(FileInfo fileInfo) {
+    private void addToProcessMap(FileInfo fileInfo) {
         String gaId = fileInfo.getGroupId() + ":" + fileInfo.getArtifactId();
         if (PROCESS_MAP.get(gaId) == null) {
             PROCESS_MAP.put(gaId, new HashSet<>());
@@ -191,22 +178,22 @@ public class CleanM2 {
         PROCESS_MAP.get(gaId).add(fileInfo.getParentFileInfo());
     }
 
-    private static DeleteReason checkForcedDeleteReason(FileInfo fileInfo) {
-        if (argData.isDeleteJavadoc() && fileInfo.getFile().getName().contains(fileInfo.getVersion() + "-javadoc.jar")) {
+    private DeleteReason checkForcedDeleteReason(FileInfo fileInfo) {
+        if (deleteJavadoc && fileInfo.getFile().getName().contains(fileInfo.getVersion() + "-javadoc.jar")) {
             return DeleteReason.FORCED_JAVADOC;
-        } else if (argData.isDeleteSource() && fileInfo.getFile().getName().contains(fileInfo.getVersion() + "-sources.jar")) {
+        } else if (deleteSource && fileInfo.getFile().getName().contains(fileInfo.getVersion() + "-sources.jar")) {
             return DeleteReason.FORCED_SOURCE;
-        } else if (argData.isDeleteAllSnapshots() && fileInfo.getVersion().endsWith("-SNAPSHOT")) {
+        } else if (deleteAllSnapshots && fileInfo.getVersion().endsWith("-SNAPSHOT")) {
             return DeleteReason.FORCED_SNAPSHOT;
-        } else if (argData.getForceArtifacts() != null && argData.getForceArtifacts().contains(fileInfo.getGroupId() + ":" + fileInfo.getArtifactId())) {
+        } else if (forceArtifacts != null && forceArtifacts.contains(fileInfo.getGroupId() + ":" + fileInfo.getArtifactId())) {
             return DeleteReason.FORCED_ARTIFACT;
-        } else if (argData.getForceGroups() != null && argData.getForceGroups().contains(fileInfo.getGroupId())) {
+        } else if (forceGroups != null && forceGroups.contains(fileInfo.getGroupId())) {
             return DeleteReason.FORCED_GROUP;
         }
         return null;
     }
 
-    private static FileInfo createFileInfo(File file) {
+    private FileInfo createFileInfo(File file) {
         FileInfo fileInfo = new FileInfo();
         fileInfo.setFile(file);
         fileInfo.setArtifactId(findArtifactId(file));
@@ -215,27 +202,27 @@ public class CleanM2 {
         return fileInfo;
     }
 
-    private static boolean checkIgnoredFile(FileInfo fileInfo) {
-        if (argData.getIgnoreArtifacts() != null && argData.getIgnoreArtifacts().contains(fileInfo.getGroupId() + ":" + fileInfo.getArtifactId())) {
+    private boolean checkIgnoredFile(FileInfo fileInfo) {
+        if (ignoreArtifacts != null && ignoreArtifacts.contains(fileInfo.getGroupId() + ":" + fileInfo.getArtifactId())) {
             SKIP_MAP.get(SkipReason.IGNORED_ARTIFACT).add(fileInfo.getParentFileInfo().getParentFileInfo().getFile().getAbsolutePath());
             return true;
-        } else if (argData.getIgnoreGroups() != null && argData.getIgnoreGroups().contains(fileInfo.getGroupId())) {
+        } else if (ignoreGroups != null && ignoreGroups.contains(fileInfo.getGroupId())) {
             SKIP_MAP.get(SkipReason.IGNORED_GROUP).add(fileInfo.getParentFileInfo().getParentFileInfo().getParentFileInfo().getFile().getAbsolutePath());
             return true;
         }
         return false;
     }
 
-    private static String findGroupId(File file, String artifactId) {
+    private String findGroupId(File file, String artifactId) {
         List<String> split = Arrays.asList(StringUtils.split(file.getAbsolutePath(), File.separatorChar));
         return StringUtils.join(split.stream().skip(split.indexOf("repository") + 1).limit(split.lastIndexOf(artifactId) - split.indexOf("repository") - 1).collect(Collectors.toList()), ".");
     }
 
-    private static String findArtifactId(File file) {
+    private String findArtifactId(File file) {
         return file.getParentFile().getParentFile().getName();
     }
 
-    private static String findVersion(File file) {
+    private String findVersion(File file) {
         return file.getParentFile().getName();
     }
 
